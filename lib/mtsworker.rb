193f0c073fc9fecb8be2683821546c45deb174e5
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-20 13:21:35 +0300

    fixed syntax

--- lib/mtsworker.rb
+++ lib/mtsworker.rb
@@ -1,6 +1,32 @@
-require "mtsworker/version"
+class Mtsworker
+   include Sneakers::Worker
+   QUEUE_NAME = ""
 
-module Mtsworker
-  class Error < StandardError; end
-  # Your code goes here...
+  from_queue QUEUE_NAME,
+   exchange: 'unifiedfeed',
+   # exchange_type: :topic,
+   :exchange_options => {
+      :type => :topic,
+      :durable => true,
+      :passive => true,
+      :auto_delete => false,
+   },
+   :queue_options => {
+      # :durable => false,
+      # :auto_delete => false,
+      # :exclusive => true,
+      # :passive => true
+   },
+   routing_key: ["*.*.*.*.*.*.*.-.#","*.*.*.*.*.*.*.#{ENV['NODE_ID']}.#"],
+   heartbeat: 5
+
+   def work_with_params(payload, delivery_info, metadata)
+     
+   rescue StandardError => e
+
+      reject!
+   end
+
+   
 end
+
