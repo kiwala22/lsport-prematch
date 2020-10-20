d50dbcca3bff809526a7c9e0285a7b9eb256eb26
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-20 13:24:10 +0300

    fixed syntax

--- lib/mtsworker.rb
+++ lib/mtsworker.rb
@@ -1,32 +1,35 @@
-class Mtsworker
-   include Sneakers::Worker
-   QUEUE_NAME = ""
+require "mtsworker/version"
 
-  from_queue QUEUE_NAME,
-   exchange: 'unifiedfeed',
+module Mtsworker
+	class Error < StandardError; end
+
+	include Sneakers::Worker
+	QUEUE_NAME = ""
+
+	from_queue QUEUE_NAME,
+	exchange: 'unifiedfeed',
    # exchange_type: :topic,
    :exchange_options => {
-      :type => :topic,
-      :durable => true,
-      :passive => true,
-      :auto_delete => false,
+   	:type => :topic,
+   	:durable => true,
+   	:passive => true,
+   	:auto_delete => false,
    },
    :queue_options => {
       # :durable => false,
       # :auto_delete => false,
       # :exclusive => true,
       # :passive => true
-   },
-   routing_key: ["*.*.*.*.*.*.*.-.#","*.*.*.*.*.*.*.#{ENV['NODE_ID']}.#"],
-   heartbeat: 5
+  },
+  routing_key: ["*.*.*.*.*.*.*.-.#","*.*.*.*.*.*.*.#{ENV['NODE_ID']}.#"],
+  heartbeat: 5
 
-   def work_with_params(payload, delivery_info, metadata)
-     
-   rescue StandardError => e
+  def work_with_params(payload, delivery_info, metadata)
 
-      reject!
-   end
+  rescue StandardError => e
 
-   
+  	reject!
+  end
+
+ 
 end
-
