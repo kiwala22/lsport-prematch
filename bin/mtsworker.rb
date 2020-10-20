9682d0e2d5b80178a2d230433e08b321218d6139
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-10-20 13:19:55 +0300

    fixed syntax

--- bin/mtsworker.rb
+++ bin/mtsworker.rb
@@ -9,12 +9,11 @@ require 'statsd-ruby'
 
 
 #statsd = Statsd.new(ENV['STATSD_HOST'], 9125)
-3Sneakers.configure(:heartbeat => 5,:amqp => 'amqp://skyline_skyline:EygMKSpVzX9MIUWO@mtsgate-ci.betradar.com:5671',:vhost => '/skyline_skyline',:log  => '/var/www/html/sportsbook/shared/log/sneakers.log', :daemonize => false, :log => STDOUT, :metrics => Sneakers::Metrics::StatsdMetrics.new(statsd))
+#Sneakers.configure(:heartbeat => 5,:amqp => 'amqp://skyline_skyline:EygMKSpVzX9MIUWO@mtsgate-ci.betradar.com:5671',:vhost => '/skyline_skyline',:log  => '/var/www/html/sportsbook/shared/log/sneakers.log', :daemonize => false, :log => STDOUT, :metrics => Sneakers::Metrics::StatsdMetrics.new(statsd))
 
 
 Sneakers.configure(:heartbeat => 5,:amqp => 'amqp://skyline_skyline:EygMKSpVzX9MIUWO@mtsgate-ci.betradar.com:5671',:vhost => '/skyline_skyline',:log  => '/var/www/html/sportsbook/shared/log/sneakers.log', :daemonize => false, :log => STDOUT)
 Sneakers.logger.level = Logger::INFO
 
-r = Sneakers::Runner.new([ mtsworker ])
+r = Sneakers::Runner.new([ MtsWorker ])
 r.run
-
